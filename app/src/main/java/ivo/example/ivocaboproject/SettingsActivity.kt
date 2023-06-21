ed0b366a7f5dab3885a353cbd775c206f597ab89
Koray Otağ <korayotag@gmail.com>
2023-06-21 12:38:39 +0300

    202306211238

--- app/src/main/java/ivo/example/ivocaboproject/SettingsActivity.kt
+++ app/src/main/java/ivo/example/ivocaboproject/SettingsActivity.kt
@@ -8,8 +8,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -20,6 +23,9 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.BottomSheetScaffoldState
+import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -30,9 +36,13 @@ import androidx.compose.material3.ListItemColors
 import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -40,13 +50,17 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.modifier.modifierLocalMapOf
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.parse.ParseUser
 import ivo.example.ivocaboproject.database.localdb.UserViewModel
 import ivo.example.ivocaboproject.ui.theme.IvocaboProjectTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 class SettingsActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -57,7 +71,7 @@ class SettingsActivity : ComponentActivity() {
 
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    contentColor=MaterialTheme.colorScheme.secondaryContainer,
+                    contentColor = MaterialTheme.colorScheme.secondaryContainer,
                     color = MaterialTheme.colorScheme.secondaryContainer
                 ) {
                     Settings()
@@ -66,20 +80,23 @@ class SettingsActivity : ComponentActivity() {
         }
     }
 }
-
+@OptIn(ExperimentalMaterial3Api::class)
+lateinit var profileSheetScaffoldState:BottomSheetScaffoldState
 @SuppressLint("RememberReturnType")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Settings(userViewModel: UserViewModel = hiltViewModel()) {
+    var scope= rememberCoroutineScope()
     val context = LocalContext.current.applicationContext
     val user = userViewModel.getUserDetail
-
+    profileSheetScaffoldState = rememberBottomSheetScaffoldState(SheetState(false))
     Column(
         Modifier
             .background(MaterialTheme.colorScheme.secondaryContainer)
-            .verticalScroll(rememberScrollState())) {
+            .verticalScroll(rememberScrollState())
+    ) {
         Text(
-            modifier= Modifier
+            modifier = Modifier
                 .fillMaxWidth()
                 .padding(15.dp),
             text = context.getString(R.string.prf_settings_title),
@@ -112,8 +129,8 @@ fun Settings(userViewModel: UserViewModel = hiltViewModel()) {
             },
             trailingContent = {
                 IconButton(onClick = {
-                    val int = Intent(context, ProfileActivity::class.java).apply {
-                        context.startActivity(this)
+                    scope.launch {
+                        profileSheetScaffoldState.bottomSheetState.expand()
                     }
                 }) {
                     Icon(
@@ -142,9 +159,9 @@ fun Settings(userViewModel: UserViewModel = hiltViewModel()) {
             },
             trailingContent = {
                 IconButton(onClick = {
-                    val int = Intent(context, ProfileActivity::class.java).apply {
+                    /*val int = Intent(context, ProfileActivity::class.java).apply {
                         context.startActivity(this)
-                    }
+                    }*/
                 }) {
                     Icon(
                         Icons.Filled.KeyboardArrowRight,
@@ -172,9 +189,9 @@ fun Settings(userViewModel: UserViewModel = hiltViewModel()) {
             },
             trailingContent = {
                 IconButton(onClick = {
-                    val int = Intent(context, ProfileActivity::class.java).apply {
+                    /*val int = Intent(context, ProfileActivity::class.java).apply {
                         context.startActivity(this)
-                    }
+                    }*/
                 }) {
                     Icon(
                         Icons.Filled.KeyboardArrowRight,
@@ -193,9 +210,9 @@ fun Settings(userViewModel: UserViewModel = hiltViewModel()) {
             },
             trailingContent = {
                 IconButton(onClick = {
-                    val int = Intent(context, ProfileActivity::class.java).apply {
+                    /*val int = Intent(context, ProfileActivity::class.java).apply {
                         context.startActivity(this)
-                    }
+                    }*/
                 }) {
                     Icon(
                         Icons.Filled.KeyboardArrowRight,
@@ -214,9 +231,9 @@ fun Settings(userViewModel: UserViewModel = hiltViewModel()) {
             },
             trailingContent = {
                 IconButton(onClick = {
-                    val int = Intent(context, ProfileActivity::class.java).apply {
+                    /*val int = Intent(context, ProfileActivity::class.java).apply {
                         context.startActivity(this)
-                    }
+                    }*/
                 }) {
                     Icon(
                         Icons.Filled.KeyboardArrowRight,
@@ -244,9 +261,9 @@ fun Settings(userViewModel: UserViewModel = hiltViewModel()) {
             },
             trailingContent = {
                 IconButton(onClick = {
-                    val int = Intent(context, ProfileActivity::class.java).apply {
+                    /*val int = Intent(context, ProfileActivity::class.java).apply {
                         context.startActivity(this)
-                    }
+                    }*/
                 }) {
                     Icon(
                         Icons.Filled.KeyboardArrowRight,
@@ -265,9 +282,9 @@ fun Settings(userViewModel: UserViewModel = hiltViewModel()) {
             },
             trailingContent = {
                 IconButton(onClick = {
-                    val int = Intent(context, ProfileActivity::class.java).apply {
+                    /*val int = Intent(context, ProfileActivity::class.java).apply {
                         context.startActivity(this)
-                    }
+                    }*/
                 }) {
                     Icon(
                         Icons.Filled.KeyboardArrowRight,
@@ -278,6 +295,58 @@ fun Settings(userViewModel: UserViewModel = hiltViewModel()) {
         )
         Divider(thickness = 1.dp)
     }
+    Profile()
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Profile(userViewModel: UserViewModel = hiltViewModel()) {
+    val context= LocalContext.current.applicationContext
+    var user=userViewModel.getUserDetail
+    BottomSheetScaffold(
+        modifier=Modifier.padding(15.dp),
+        scaffoldState = profileSheetScaffoldState,
+        sheetPeekHeight = 0.dp,
+        sheetContent = {
+        Column(modifier=Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(
+                text=context.getString(R.string.prf_profile_title).uppercase(),
+                modifier=Modifier.fillMaxWidth(),
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Bold, textAlign = TextAlign.Center)
+
+            Divider()
+            Column(Modifier.padding(15.dp),horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(text = context.getString(R.string.rg_username)+" : "+user.username)
+                Text(text = context.getString(R.string.email)+" : "+user.email)
+                Row(horizontalArrangement = Arrangement.Center, modifier = Modifier.padding(0.dp,20.dp)) {
+                    Button(onClick = {
+                        ParseUser.logOut()
+
+                    }) {
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.baseline_logout_24),
+                                contentDescription = null
+                            )
+                            Text(text = context.getString(R.string.logout))
+                        }
+                    }
+                    Spacer(modifier = Modifier.weight(2f))
+                    Button(onClick = { /*TODO*/ }) {
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.baseline_delete_forever_24),
+                                contentDescription = null
+                            )
+                            Text(text = context.getString(R.string.remove))
+                        }
+                    }
+                }
+            }
+
+        }
+    }) {}
 }
 
 @Preview(showBackground = true)
